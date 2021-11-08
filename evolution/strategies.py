dd8fa60eb80ad372340e897addafac1cf7efc212
baton96 <podolce@op.pl>
2021-11-08 10:37:03 +0100

    Refactored evolution strategies

--- evolution/strategies.py
+++ evolution/strategies.py
@@ -1,13 +1,11 @@
-from numpy import empty, argsort, sqrt, cov, reshape, floor, repeat, newaxis, argmin, array
-from numpy.random import seed, uniform, multivariate_normal, normal, randint
-from numpy.linalg import norm
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.metrics.pairwise import pairwise_distances_argmin
-from numpy.linalg import norm
 from time import time
 
-seed(0)
+import numpy as np
+import numpy.random as rnd
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+
+rnd.seed(0)
 
 nBombs = 3
 kMeans = KMeans(n_clusters=nBombs)
@@ -15,19 +13,18 @@ populationSize = 150
 nParents = 20
 nOffspring = 150
 nGenerations = 10
-dMax = sqrt(20000)
+dMax = np.sqrt(20000)
 nNests = 12
-wasps = randint(low=100, high=1000, size=nNests)
+wasps = rnd.randint(low=100, high=1000, size=nNests)
 allWasps = sum(wasps)
-positions = uniform(low=0, high=100, size=(nNests, 2))
-origin = uniform(100, size=(populationSize, nBombs, 2))
+positions = rnd.uniform(low=0, high=100, size=(nNests, 2))
+origin = rnd.uniform(100, size=(populationSize, nBombs, 2))
 population = origin[:]
-clusters = empty((nBombs, nOffspring, 2))
-fitness = empty(populationSize)
+clusters = np.empty((nBombs, nOffspring, 2))
+fitness = np.empty(populationSize)
 start = time()
 
-'''
-# verbose Simple Evolution strategy
+# Simple Evolution strategy
 for _ in range(nGenerations):
     # all bomb co-ordinates of currently processed solution  
     for i, bombs in enumerate(population):
@@ -36,36 +33,23 @@ for _ in range(nGenerations):
         # co-ordinates of currently processed bomb
         for coords in bombs:
             # distances between each nest and currently processed bomb
-            distances = norm(positions-coords, axis=1)
+            distances = np.linalg.norm(positions - coords, axis=1)
             # information about what part of wasps in each nest will remain after the bomb
-            parts = distances/dMax
+            parts = distances / dMax
             # updating numbers of wasps in each nest
-            tmpWasps = floor(parts*tmpWasps)
+            tmpWasps = np.floor(parts * tmpWasps)
         # fitness of currently proccessed solution
         fitness[i] = sum(tmpWasps)
     # index of the single best solution
-    maxId = argmin(fitness)
+    maxId = np.argmin(fitness)
     # single best solution
     parents = population[maxId]
     # nOffspring clones
-    clones = repeat(parents[newaxis], nOffspring, axis=0)
+    clones = np.repeat(parents[np.newaxis], nOffspring, axis=0)
     # offspring created by adding noise to clones
-    population = clones + normal(size=(nOffspring,nBombs,2))
-'''
+    population = clones + rnd.normal(size=(nOffspring, nBombs, 2))
 
-'''
-# compact Simple Evolution strategy
-for _ in range(nGenerations):
-    for i, bombs in enumerate(population):
-        tmpWasps = wasps[:]
-        for coords in bombs:
-            tmpWasps = floor(tmpWasps*norm(positions-coords, axis=1)/dMax)
-        fitness[i] = sum(tmpWasps)
-    population = repeat(population[argmin(fitness)][newaxis], nOffspring, axis=0) + normal(size=(nOffspring,nBombs,2))
-'''
-
-'''
-# verbose Simple Genetic strategy
+# Simple Genetic strategy
 for _ in range(nGenerations):
     # all bomb co-ordinates of currently processed solution  
     for i, bombs in enumerate(population):
@@ -74,61 +58,47 @@ for _ in range(nGenerations):
         # co-ordinates of currently processed bomb
         for coords in bombs:
             # distances between each nest and currently processed bomb
-            distances = norm(positions-coords, axis=1)
+            distances = np.norm(positions - coords, axis=1)
             # information about what part of wasps in each nest will remain after the bomb
-            parts = distances/dMax
+            parts = distances / dMax
             # updating numbers of wasps in each nest
-            tmpWasps = floor(parts*tmpWasps)
+            tmpWasps = np.floor(parts * tmpWasps)
         # fitness of currently proccessed solution
         fitness[i] = sum(tmpWasps)
     # indices that would sort an array
-    indices = argsort(fitness)
+    indices = np.argsort(fitness)
     # indices of the nParents best solutions
     parentIds = indices[:nParents]
     # nParents best solutions indicated by parentIndices
     parents = population[parentIds]
     # container for nOffspring solutions derived from-parents
-    offspring = empty((nOffspring,nBombs,2))    
+    offspring = np.empty((nOffspring, nBombs, 2))
     for offspringId in range(nOffspring):
         # parents used to create new offspring solution
-        mom = parents[offspringId%nParents]
-        dad = parents[(offspringId+1)%nParents]     
+        mom = parents[offspringId % nParents]
+        dad = parents[(offspringId + 1) % nParents]
         # temporary container for offspring solution
-        tmpOffspring = empty((3,2))
+        tmpOffspring = np.empty((3, 2))
         # co-ordinates of currently processed mom bomb
         for i, coords in enumerate(mom):
             # distances between each dad bomb and currently processed mom bomb
-            distances = norm(coords-dad, axis=1)
+            distances = np.norm(coords - dad, axis=1)
             # index of the dad bomb nearest to the currently processed mom bomb
-            nearestId = argmin(distances)
+            nearestId = np.argmin(distances)
             # dad bomb nearest to the currently processed mom bomb
             nearest = dad[nearestId]
             # child bomb derived from crossover between parent bombs
-            childBomb = ( nearest + coords)/2
+            childBomb = (nearest + coords) / 2
             # child bomb mutated by adding sample from a normal (Gaussian) distribution
-            mutated = childBomb + normal(size=2)
+            mutated = childBomb + rnd.normal(size=2)
             # filling temporary offspring container with bombs
             tmpOffspring[i] = mutated
         # filling offspring container with ready solutions
-        offspring[offspringId] = tmpOffspring       
+        offspring[offspringId] = tmpOffspring
     population = offspring
     # population = concatenate((parents,offspring))
-'''
 
-'''
-# compact Simple Genetic strategy
-for _ in range(nGenerations):
-    for i, bombs in enumerate(population):
-        tmpWasps = wasps[:]
-        for coords in bombs:
-            tmpWasps = floor(tmpWasps*norm(positions-coords, axis=1)/dMax)
-        fitness[i] = sum(tmpWasps)
-    parents = population[argsort(fitness)[:nParents]]
-    population = array([[(parents[(i+1)%nParents][argmin(norm(coords-parents[(i+1)%nParents], axis=1))]+coords)/2.0+normal(size=2) for coords in parents[i%nParents]] for i in range(nOffspring)])
-'''
-
-'''
-# verbose Covariance-Matrix Adaptation Evolution strategy (CMA-ES)
+# Covariance-Matrix Adaptation Evolution strategy (CMA-ES)
 for _ in range(nGenerations):
     # all bomb co-ordinates of currently processed solution  
     for i, bombs in enumerate(population):
@@ -137,21 +107,21 @@ for _ in range(nGenerations):
         # co-ordinates of currently processed bomb
         for coords in bombs:
             # distances between each nest and currently processed bomb
-            distances = norm(positions-coords, axis=1)
+            distances = np.norm(positions - coords, axis=1)
             # information about what part of wasps in each nest will remain after the bomb
-            parts = distances/dMax
+            parts = distances / dMax
             # updating numbers of wasps in each nest
-            tmpWasps = floor(parts*tmpWasps)
+            tmpWasps = np.floor(parts * tmpWasps)
         # fitness of currently proccessed solution
         fitness[i] = sum(tmpWasps)
     # indices that would sort an array
-    indices = argsort(fitness)
+    indices = np.argsort(fitness)
     # indices of the nParents best solutions
     parentIds = indices[:nParents]
     # nParents best solutions indicated by parentIndices
     parents = population[parentIds]
     # flattened population, co-ordinates of all bombs
-    flatPopulation = reshape(parents,(-1,2))
+    flatPopulation = np.reshape(parents, (-1, 2))
     # tool used to group bombs according to their co-ordinates
     kMeans = KMeans(n_clusters=nBombs).fit(flatPopulation)
     # centers of each group
@@ -162,31 +132,14 @@ for _ in range(nGenerations):
         # all bomb belonging to the group i
         cluster = flatPopulation[labels == i]
         # covariance matrix within cluster
-        covMatrix = cov(cluster,rowvar=False)
+        covMatrix = np.cov(cluster, rowvar=False)
         # new bomb co-ordinates sampled from a multivariate normal distribution
-        clusters[i] = multivariate_normal(centers[i], covMatrix, nOffspring)
+        clusters[i] = rnd.multivariate_normal(centers[i], covMatrix, nOffspring)
     # container for nOffspring solutions from combining bomb groups
-    population = empty((nOffspring,nBombs,2))
+    population = np.empty((nOffspring, nBombs, 2))
     # each new individual takes one bomb from each group
     for i, bombs in enumerate(zip(*clusters)):
         population[i] = bombs
-'''
-
-'''
-# compacted Covariance-Matrix Adaptation Evolution strategy (CMA-ES)
-for _ in range(nGenerations):
-    fitness = empty(len(population))
-    for i, bombs in enumerate(population):
-        tmpWasps = wasps[:]
-        for coords in bombs:
-            tmpWasps = floor(tmpWasps*norm(positions-coords, axis=1)/dMax)
-        fitness[i] = sum(tmpWasps)
-    flatPopulation = reshape(population[argsort(fitness)[:nParents]],(-1,2))
-    kMeans = KMeans(n_clusters=nBombs).fit(flatPopulation)
-    labels = pairwise_distances_argmin(flatPopulation, kMeans.cluster_centers_)
-    clusters = [multivariate_normal(kMeans.cluster_centers_[i], cov(flatPopulation[labels==i],rowvar=False), nOffspring) for i in range(nBombs)]
-    population = array([array(bombs) for bombs in zip(*clusters)])
-'''
 
 print("Excecution took:", round(time() - start, 3), "s")
 print("Number of wasps killed:", allWasps - int(min(fitness)))
